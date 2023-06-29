6bf511dc9ec28c4fad48d11e7e850d2c01eb6473
Said Unger <said.unger@googlemail.com>
2023-06-29 08:19:00 +0200

    upload first assignment

---
+++ 01-intro/R/assignment.R
@@ -0,0 +1,31 @@
+#### additional training ####
+
+# import the "midwest" dataset from ggplot2 (tidyverse package)
+
+# briefly describe what the dataframe is generally about (no statististics)
+
+# how many states and counties are in the data?
+
+# calculate all population statistics on the state level
+# you can create a new dataframe for each step or do it in one go
+
+## 1. add population counts and are grouped by state
+## hints: use "group_by" and "summarise" with "across" and "everything()"
+## also: you are allowed to use the internet for help!
+
+## 2. build a function to calculate percentages
+## inputs: total population and part of population
+## output: the percentage value
+
+## 3. produce a new colum with statewide percentages
+## for all populations
+
+### a) do this with the function defined above
+
+### b) do this within tidy
+
+# which state has the highest percentage of inhabitants identifying as
+# amerindian?
+
+# in preparation for the next workshop:
+## go through the cleaning script (03-cleaning_viz.R) by yourselves!
